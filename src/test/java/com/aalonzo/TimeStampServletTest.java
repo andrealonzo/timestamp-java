ba738a19af55eee903e0d39d97d4a511a29d8a37
Andre Alonzo <aalonzo@192.168.1.122>
2016-03-16 13:50:09 -0700

    updated unit test

--- src/test/java/com/aalonzo/TimeStampServletTest.java
+++ src/test/java/com/aalonzo/TimeStampServletTest.java
@@ -12,25 +12,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TimeStampServletTest {
+	HttpServletResponse response;
+	HttpServletRequest request;
+	StringWriter stringWriter;
+	PrintWriter writer;
 
+   @Before
+   public  void setUp() throws IOException{
+		//create mock objects
+		 response = mock(HttpServletResponse.class);
+		 request = mock(HttpServletRequest.class);
+		 stringWriter = new StringWriter();
+		 writer = new PrintWriter(stringWriter);
+	     when(response.getWriter()).thenReturn(writer);
+   }
 	@Test
 	public void testUnixDate() throws ServletException, IOException {
 		String unixDate = "1450137600";
 		String expectedOutput = "{\"unixDate\":1450137600,\"naturalLanguageDate\":\"December 15, 2015\"}\n";
 
-		//create mock objects
-		HttpServletResponse response = mock(HttpServletResponse.class);
-		HttpServletRequest request = mock(HttpServletRequest.class);
-
 		when(request.getPathInfo()).thenReturn("/" + unixDate);
-		StringWriter stringWriter = new StringWriter();
-		PrintWriter writer = new PrintWriter(stringWriter);
-		
-        when(response.getWriter()).thenReturn(writer);
-        
+   
         //call servlet with mock objects
         new TimeStampServlet().doGet(request, response);
         
@@ -41,15 +47,7 @@ public class TimeStampServletTest {
 		String unixDate = "-601084800";
 		String expectedOutput = "{\"unixDate\":-601084800,\"naturalLanguageDate\":\"December 15, 1950\"}\n";
 
-		//create mock objects
-		HttpServletResponse response = mock(HttpServletResponse.class);
-		HttpServletRequest request = mock(HttpServletRequest.class);
-
 		when(request.getPathInfo()).thenReturn("/" + unixDate);
-		StringWriter stringWriter = new StringWriter();
-		PrintWriter writer = new PrintWriter(stringWriter);
-		
-        when(response.getWriter()).thenReturn(writer);
         
         //call servlet with mock objects
         new TimeStampServlet().doGet(request, response);
@@ -62,15 +60,7 @@ public class TimeStampServletTest {
 		String naturalLanguageDate = "December 15, 2015";
 		String expectedOutput = "{\"unixDate\":1450137600,\"naturalLanguageDate\":\"December 15, 2015\"}\n";
 
-		//create mock objects
-		HttpServletResponse response = mock(HttpServletResponse.class);
-		HttpServletRequest request = mock(HttpServletRequest.class);
-
 		when(request.getPathInfo()).thenReturn("/" + naturalLanguageDate);
-		StringWriter stringWriter = new StringWriter();
-		PrintWriter writer = new PrintWriter(stringWriter);
-		
-        when(response.getWriter()).thenReturn(writer);
         
         //call servlet with mock objects
         new TimeStampServlet().doGet(request, response);
